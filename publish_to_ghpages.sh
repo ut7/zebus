2c6d75344888f8db9d582bf05febffefdc52c929
Étienne Charignon <etienne.charignon@gmail.com>
2020-08-03 09:42:26 +0200

    ne push gh-pages que s'il y a eu un nouveau commit

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -21,10 +21,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Deployement du site ($0)"
-
-echo "Pushing to github"
-git push --all
-
-cd ..
+echo "Updating gh-pages branch and publishing"
+cd public && git add --all && git commit -m "Deployement du site ($0)" && git push
